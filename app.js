2478468e01b959c7ab1a17a6dc069aa423445977
Angus Mclean <48145878+AngusMclean24@users.noreply.github.com>
2021-11-24 11:50:19 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const initialScreen = document.getElementById('initialScreen');
 const lobbyScreen = document.getElementById('lobbyScreen');
+const loadingScreen = document.getElementById('loadingScreen');
 const joinForm = document.getElementById('joinForm');
 const inputForm = document.getElementById('inputField');
 const joinLink = document.getElementById('joinLink');
 const userList = document.getElementById('users');
-
+const copyButton = document.getElementById('copyButton');
 const joinGameBtn = document.getElementById('joinGameButton');
 
 //Get username and room from URL
@@ -14,19 +15,33 @@ const { username, room } = Qs.parse(location.search, {
 });
 */
 //location.href = "./index.html?room=hubbabuuba";
-
 const ADDRESS = "https://angusmclean24.github.io/io-game/";
 const URL = "wss://slime-io-game.glitch.me/";
 const socket = io(URL);
 
+
 //User info
-var username = null
-var sessionID = null
-var room = null
+var username = null;
+var sessionID = null;
+var room = null;
+var host = false;
 
 //messages from server
+
+//when connection is established
 socket.on('connect', function() {
     sessionID = socket.id;
+    showLobbyScreen();
+});
+
+//if connection lost
+socket.on('disconnect', function() {
+    socket.disconnect();
+    showInitialScreen();
+    username = null;
+    sessionID = null;
+    room = null;
+    host = false;
 });
 
 socket.on('room', function(room){
@@ -36,8 +51,12 @@ socket.on('room', function(room){
 
 socket.on('roomUsers', (users) => {
     outputUsers(users);
+    console.log(users);
 });
 
+socket.on('makeHost', (ishost) => {
+    host = ishost
+});
 
 //Dom input listeners
 joinForm.addEventListener("submit", function(evt) {
@@ -49,18 +68,18 @@ joinForm.addEventListener("submit", function(evt) {
     if (username == ""){
         inputForm.classList.add("is-invalid");
     } else {
+        //can connect
+        socket.connect();
+        showLoadingScreen();
+        room = location.search.replace("?","");
+
         //if host
         if (buttonClicked == "host") {
-            socket.emit('getRoom', { username });
-        } else {
-        //if trying to join specific room
-            room = location.search.replace("?","");;
-            displayJoinLink(room);
-            socket.emit('joinRoom', { username, room })
-        } 
-        joinLobby()
-    }
-    
+            host = true
+        }
+
+        socket.emit('sendUser', { username, room, host });
+    }  
 }, true);
 
 //copyButton.addEventListener("click", console.log("hello"));
@@ -68,9 +87,22 @@ joinForm.addEventListener("submit", function(evt) {
 
 
 //Dom functions
-function joinLobby (){
+function showLoadingScreen(){
     initialScreen.style.display = "none";
+    loadingScreen.style.display = "block";   
+    lobbyScreen.style.display = "none";   
+}
+
+function showLobbyScreen (){
+    initialScreen.style.display = "none";
+    loadingScreen.style.display = "none";   
     lobbyScreen.style.display = "block";   
+}
+
+function showInitialScreen (){
+    initialScreen.style.display = "block";
+    loadingScreen.style.display = "none";   
+    lobbyScreen.style.display = "none";   
 }
 
 function displayJoinLink (room) {
@@ -93,6 +125,10 @@ function outputUsers(users){
     var text = null
     for (let i =0; i < users.length; i++){
         var toplace = users[i].username;
+        if (users[i].ishost) {
+            toplace += " (host)";
+        }
+
         if (users[i].id == sessionID) {
             toplace += " (you)";
         }
@@ -106,4 +142,6 @@ function outputUsers(users){
     }
     userList.innerHTML = text;
 }   
+
+
  
